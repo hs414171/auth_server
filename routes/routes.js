fc347b20a04f49f7f46c5d5e7146f95ebbe2fff2
hs414171 <hs414171@gmail.com>
2021-07-31 19:56:28 +0530

    hi

--- routes/routes.js
+++ routes/routes.js
@@ -14,7 +14,7 @@ const { valid } = require('@hapi/joi')
 router.get('/get-users', async (req,res)=>{
     try{
         const userData =  await User.find()
-        res.send(userData)
+        res.json(userData)
     }catch(error){
         res.status(500).json({message: error.message})
     }
