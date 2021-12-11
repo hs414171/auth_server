aebfa072024ce5db94b02b3eb4780eca308d2fb6
hs414171 <hs414171@gmail.com>
2021-12-11 09:56:28 +0530

    lmao

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const user_routes = require('./routes/routes')
-const prod_routes = require('./routes/product_routes')
+// const prod_routes = require('./routes/product_routes')
 
 const cors = require('cors')
 const port = process.env.PORT || 3000
@@ -33,7 +33,7 @@ db.on('error', (error)=>{console.log(error)});
 db.once('open', ()=>{console.log('Connected to Database')});
 app.use(express.json())
 app.use('/api/user',user_routes)
-app.use('/api/product',prod_routes)
+// app.use('/api/product',prod_routes)
 
 
 
