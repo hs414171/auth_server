3e367389fed67ff50a709861f0f77f180ff025a5
hs414171 <hs414171@gmail.com>
2021-12-26 13:34:58 +0530

    go daddy

--- nodmailer.js
+++ nodmailer.js
@@ -1,10 +1,22 @@
 require("dotenv").config()
 const nodemailer = require('nodemailer')
-var transport = nodemailer.createTransport({
-    service: "hotmail",
+// var transport = nodemailer.createTransport({
+//     service: "hotmail",
+//     auth: {
+//         user: process.env.EMAIL_ADDRESS,
+//         pass: process.env.EMAIL_PASSWORD
+//     }
+// });
+// module.exports = transport
+var transport = nodemailer.createTransport({    
+    service: 'Godaddy',
+    host: "smtpout.secureserver.net",  
+    secureConnection: true,
+    port: 465,
+
     auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD
-    }
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASSWORD
+            }
 });
 module.exports = transport
